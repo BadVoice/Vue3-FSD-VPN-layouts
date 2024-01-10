ac9da938c0255c22b19b5dce70a68793ad8d2267
German <german.suvorov134@gmail.com>
2024-01-10 15:27:29 +0300

    ref: project structure

---
+++ src/entities/user/api/types.ts
@@ -0,0 +1,5 @@
+export type UserDto = {
+  id: string
+  email: string
+  password: string
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import '@/app/main.css'
+
+import App from '@/app/App.vue'
+import router from '@/app/router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
